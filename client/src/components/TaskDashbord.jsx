25c7adf05564f31f70f2d3f2717eb7c376e36423
tarasinghrajput <tarasinghrajput7261@gmail.com>
2025-12-15 09:38:10 +0530

    add the tailwindcss and shadcn

--- client/src/components/TaskDashbord.jsx
+++ client/src/components/TaskDashbord.jsx
@@ -1,5 +1,6 @@
 // import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Button } from "@/components/ui/button"
 
 
 function TaskDashboard() {
@@ -7,36 +8,39 @@ function TaskDashboard() {
     if (localStorage.length > 0) {
         tasks.push(JSON.parse(localStorage.getItem('task')))
     }
-    const navigate = useNavigate()
 
     return (
-        <section className="taskDashboardSection">
-            <div className="taskDashboardList">
-                <h2>Task List</h2>
-                <hr />
-                {!tasks ?
-                    <table className="taskList" border="5">
-                        <tbody>
-                            <tr>
-                                <th>Date</th>
-                                <th>Title</th>
-                                <th>Time Elapsed</th>
-                            </tr>
-                            {tasks[0].map(task => (
-                                <tr key={task.id}>
-                                    <td>{task.taskDate}</td>
-                                    <td>{task.taskTitle}</td>
-                                    <td>{task.taskTimeElapsed}</td>
-                                </tr>
-                            ))
-                            }
-                        </tbody>
-                    </table> : <p>No Tasks Added</p>
-                }
-            </div>
 
-            <button onClick={() => navigate('/taskTimer')}>Add Task</button>
-        </section>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+            <section className="taskDashboardSection">
+                <div className="taskDashboardList">
+                    <h2>Task List</h2>
+                    <hr />
+                    {!tasks ?
+                        <table className="taskList" border="5">
+                            <tbody>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Title</th>
+                                    <th>Time Elapsed</th>
+                                </tr>
+                                {tasks[0].map(task => (
+                                    <tr key={task.id}>
+                                        <td>{task.taskDate}</td>
+                                        <td>{task.taskTitle}</td>
+                                        <td>{task.taskTimeElapsed}</td>
+                                    </tr>
+                                ))
+                                }
+                            </tbody>
+                        </table> : <p>No Tasks Added</p>
+                    }
+                </div>
+                <Button asChild>
+                    <Link to="/taskTimer">+ Add Task</Link>
+                </Button>
+            </section>
+        </div>
     )
 }
 
